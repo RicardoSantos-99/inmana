ac0344f839449a956160b72086c3c9474f200a8a
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-05 10:55:09 -0300

    feat: add evaluate

--- lib/inmana/welcomer.ex
+++ lib/inmana/welcomer.ex
@@ -1,7 +1,20 @@
 defmodule Inmana.Welcomer do
-  def welcome(params) do
-    %{"name" => name, "age" => age} = params
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
 
-    "Hello #{name}, you are #{age} years old."
+    name
+    |> String.trim()
+    |> String.capitalize()
+    |> evaluate(age)
   end
+
+  defp evaluate("Banana", 42) do
+    {:ok, "You are very special, banana!"}
+  end
+
+  defp evaluate(name, age) when age >= 18 do
+    {:ok, "Welcome, #{name}!"}
+  end
+
+  defp evaluate(name, _age), do: {:error, "You shall not pass, #{name}!"}
 end
